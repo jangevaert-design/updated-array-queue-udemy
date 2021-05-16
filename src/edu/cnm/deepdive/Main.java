aa71e48e717668be1d1adbb6bd4cebd20ebd7d04
Jan Gevaert <jan.gevaert@gmail.com>
2021-05-16 11:11:45 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/Main.java
@@ -0,0 +1,47 @@
+package edu.cnm.deepdive;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Employee janeJones = new Employee("Jane", "Jones", 123);
+    Employee johnDoe = new Employee("John", "Doe", 4567);
+    Employee marySmith = new Employee("Mary", "Smith", 22);
+    Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+    Employee billEnd = new Employee("Bill", "End", 78);
+
+    ArrayQueue queue = new ArrayQueue(5);
+
+    queue.add(janeJones);
+    queue.add(johnDoe);
+    queue.remove();
+    queue.add(marySmith);
+    queue.remove();
+    queue.add(mikeWilson);
+    queue.remove();
+    queue.add(billEnd);
+    queue.remove();
+    queue.add(janeJones);
+    System.out.println();
+    queue.printQueue();
+
+//    queue.remove();
+//    queue.remove();
+//    System.out.println();
+//    queue.printQueue();
+//
+//    System.out.println();
+//    System.out.println(queue.peek());
+//    System.out.println();
+//    queue.printQueue();
+//    queue.remove();
+//    queue.remove();
+//    queue.remove();
+//
+//    queue.add(mikeWilson);
+//
+//    System.out.println();
+//    queue.printQueue();
+
+  }
+}
